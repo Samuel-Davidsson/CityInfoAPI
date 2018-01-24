e34856527fc0d8ce55f7fa8091d3c4cb0979e08a
Samuel <Sam.Davidsson86@outlook.com>
2018-01-24 21:59:30 +0100

    Intial Commit

---
+++ CityInfo.API/CityInfo/CityInfo.Api/Controllers/CitiesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CityInfo.Api.Controllers
+{
+
+    [Route("api/cities")]
+    public class CitiesController : Controller
+    {
+        [HttpGet()]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(new List<object>()
+            {
+                new {id=1, Name="Stockholm"},
+                new {id=2, Name="Kalmar"}
+            });
+        }
+    }
+}
